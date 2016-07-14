1168b6272d84249e8a18dc1add76c36d3089bcc2
Android-he <he963023350@163.com>
2016-07-14 17:11:04 +0800

    123

--- src/com/zuzoo/zuzoomart/activity/RegisterNextActivity.java
+++ src/com/zuzoo/zuzoomart/activity/RegisterNextActivity.java
@@ -1,11 +1,14 @@
 package com.zuzoo.zuzoomart.activity;
 
 import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVMobilePhoneVerifyCallback;
 import com.avos.avoscloud.AVOSCloud;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
+import com.avos.avoscloud.AVUser;
 import com.avos.avoscloud.RequestMobileCodeCallback;
 import com.avos.avoscloud.SaveCallback;
+import com.avos.avoscloud.SignUpCallback;
 import com.zuzoo.zuzoomart.R;
 
 import android.app.Activity;
@@ -41,8 +44,8 @@ public class RegisterNextActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.activity_register_next);
 		initView();
 		phone_num = getIntent().getExtras().get("phone").toString();
-		 sp = getSharedPreferences("user",MODE_PRIVATE);
-		
+		sp = getSharedPreferences("user", MODE_PRIVATE);
+
 	}
 
 	private void initView() {
@@ -77,33 +80,85 @@ public class RegisterNextActivity extends Activity implements OnClickListener {
 			finish();
 			break;
 		case R.id.btn_get_code:
-			 AVOSCloud.requestSMSCodeInBackgroud(phone_num, new RequestMobileCodeCallback() {
-				
+			AVUser.requestMobilePhoneVerifyInBackground(phone_num, new RequestMobileCodeCallback() {
 				@Override
-				public void done(AVException arg0) {
-					Toast.makeText(getApplicationContext(), "短信已发送，请注意查收", Toast.LENGTH_SHORT).show();
+				public void done(AVException e) {
+					if (e == null) {
+						// 发送成功
+						Toast.makeText(getApplicationContext(), "验证码已发送，请注意查收短信~", Toast.LENGTH_SHORT).show();
+					} else {
+						Toast.makeText(getApplicationContext(), "验证码发送失败~", Toast.LENGTH_SHORT).show();
+					}
+
 				}
 			});
 			break;
 		case R.id.btn_register_finish:
-			 final AVObject todo = new AVObject("_User");
-		        todo.put("username", phone_num);// 设置名称
-		        todo.put("mobilePhoneNumber", phone_num);// 设置名称
-		        todo.put("password", pwd.getText().toString());// 设置名称
-		        todo.saveInBackground(new SaveCallback() {
-					
+			if (code.getText().toString().equals("")) {
+				Toast.makeText(getApplicationContext(), "请填写验证码", Toast.LENGTH_SHORT).show();
+			} else if (pwd.getText().toString().equals("")) {
+				Toast.makeText(getApplicationContext(), "请填写密码", Toast.LENGTH_SHORT).show();
+			} else {
+				AVUser.verifyMobilePhoneInBackground(code.getText().toString(), new AVMobilePhoneVerifyCallback() {
 					@Override
 					public void done(AVException e) {
-						 if (e == null) {
-			                    // 存储成功
-							 Editor editor = sp.edit(); 
-							 editor.putString("userId", todo.getObjectId());
-							 Toast.makeText(getApplicationContext(), "注册成功~", Toast.LENGTH_SHORT).show();
-			                } else {
-			                    // 失败的话，请检查网络环境以及 SDK 配置是否正确
-			                }
+						if (e == null) {
+							// 验证成功
+							AVUser user = new AVUser();
+							user.setPassword(pwd.getText().toString());
+							// 其他属性可以像其他AVObject对象一样使用put方法添加
+							user.put("mobilePhoneNumber", phone_num);
+
+							user.signUpInBackground(new SignUpCallback() {
+								public void done(AVException e) {
+									if (e == null) {
+										// successfully
+										Toast.makeText(getApplicationContext(), "注册成功~", Toast.LENGTH_SHORT).show();
+
+										final AVObject todo = new AVObject("_User");
+										todo.put("username", phone_num);// 设置名称
+										todo.put("mobilePhoneNumber", phone_num);// 设置名称
+										todo.put("password", pwd.getText().toString());// 设置名称
+										todo.saveInBackground(new SaveCallback() {
+											@Override
+											public void done(AVException e) {
+												if (e == null) {
+													// 存储成功
+													Editor editor = sp.edit();
+													editor.putString("userId", todo.getObjectId());
+												} else {
+													// 失败的话，请检查网络环境以及 SDK 配置是否正确
+												}
+											}
+										});
+									} else {
+										// failed
+									}
+								}
+							});
+
+						} else {
+							// Log.d("SMS", "Verified failed!");
+						}
 					}
 				});
+			}
+			final AVObject todo = new AVObject("_User");
+			todo.put("username", phone_num);// 设置名称
+			todo.put("mobilePhoneNumber", phone_num);// 设置名称
+			todo.put("password", pwd.getText().toString());// 设置名称
+			todo.saveInBackground(new SaveCallback() {
+				@Override
+				public void done(AVException e) {
+					if (e == null) {
+						// 存储成功
+						Editor editor = sp.edit();
+						editor.putString("userId", todo.getObjectId());
+					} else {
+						// 失败的话，请检查网络环境以及 SDK 配置是否正确
+					}
+				}
+			});
 			break;
 
 		default:
